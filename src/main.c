744429085fb2c1a7246f725878a86080c2af1080
Guillaume Bour <contact@guillaumebour.fr>
2020-12-19 10:57:50 +0100

    fix bug in main and switch to write in file.

--- src/main.c
+++ src/main.c
@@ -3,15 +3,19 @@
 #include <sys/wait.h>
 
 void parent(char * filename, int pipe) {
+    printf("Start parent\n");
     encode_text(filename, pipe);
 
-    if (wait(0) == -1) {
+    if (wait(NULL) == -1) {
         print_error_and_exit("Error while waiting");
     }
+    printf("End parent\n");
 }
 
 void child(int pipe, FILE * dest) {
+    printf("Start child\n");
     decode_text(pipe, dest);
+    printf("End child\n");
 }
 
 
@@ -25,13 +29,18 @@ int main(int argc, char *argv[]) {
         print_error_and_exit("Error while opening pipe");
     }
 
+    FILE *output_file = fopen("resultat.txt", "w+");
+    if (output_file == NULL) {
+        print_error_and_exit("Error while opening output file");
+    }
+
     int pid = fork();
     if (pid == 0) {
-        child(pfd[0], stdout);
+        child(pfd[0], output_file);
         exit(0);
     }
 
-    parent(argv[0], pfd[1]);
+    parent(argv[1], pfd[1]);
 
     return 0;
 }
